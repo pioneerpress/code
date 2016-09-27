d02cd210444c205451ce1c2366e80be16b048fc7
dhmontgomery <dhmontgomery@gmail.com>
2016-09-26 23:53:59 -0500

    Comments, condensed code, fresh images
    Added a for loop to condense the code. Fixed a minor error. Commented
    it out. Uploaded new wordcloud images to account for the minor fix.

--- 926-debate-wordclouds/wordclouds.r
+++ 926-debate-wordclouds/wordclouds.r
@@ -1,66 +1,46 @@
-require(tm)
-require(RColorBrewer)
-require(wordcloud)
-require(stringr)
+# An R script to load transcripts from the U.S. presidential debate on Monday, Sept. 26, 2016, and produce wordclouds of each word spoken by candidates Hillary Clinton and Donald Trump and moderator Lester Holt.
+# Output will be three .PNG files, one for each candidate.
+# Created by David H. Montgomery of the St. Paul Pioneer Press.
 
-clinton <- read.delim(file="Clinton.txt", sep="\n", stringsAsFactors = F)
-trump <- read.delim(file="Trump.txt", sep="\n", stringsAsFactors = F)
-holt <- read.delim(file="Holt.txt", sep="\n", stringsAsFactors = F)
+# Load necessary libraries
+library(tm)
+library(RColorBrewer)
+library(wordcloud)
+library(stringr)
+library(Hmisc)
 
-clinton <- unlist(lapply(clinton, function(x) { str_split(x, "\n")}))
-trump <- unlist(lapply(trump, function(x) { str_split(x, "\n")}))
-holt <- unlist(lapply(holt, function(x) { str_split(x, "\n")})) 
+names <- c("clinton","trump","holt") # Establish the three candidates
 
-clinton <- Corpus(VectorSource(clinton))
-trump <- Corpus(VectorSource(trump))
-holt <- Corpus(VectorSource(holt))
-
-
-clinton <- tm_map(clinton, content_transformer(tolower)) ## Make everything lowercase
-clinton <- tm_map(clinton, removeWords,stopwords("english")) ## Remove common English words like "and" and "the".
-clinton <- tm_map(clinton, removePunctuation) ## Remove punctuation
-clinton <- tm_map(clinton, stripWhitespace) ## Strip out unnecessary whitespace.
-
-
-trump <- tm_map(trump, content_transformer(tolower)) ## Make everything lowercase
-trump <- tm_map(trump, removeWords,stopwords("english")) ## Remove common English words like "and" and "the".
-trump <- tm_map(trump, removePunctuation) ## Remove punctuation
-trump <- tm_map(trump, stripWhitespace) ## Strip out unnecessary whitespace.
-
-
-holt <- tm_map(holt, content_transformer(tolower)) ## Make everything lowercase
-holt <- tm_map(holt, removeWords,stopwords("english")) ## Remove common English words like "and" and "the".
-holt <- tm_map(holt, removePunctuation) ## Remove punctuation
-holt <- tm_map(holt, stripWhitespace) ## Strip out unnecessary whitespace.
-
-clinton.tdm <- TermDocumentMatrix(clinton)
-trump.tdm <- TermDocumentMatrix(trump)
-holt.tdm <- TermDocumentMatrix(holt)
-
-png("clinton.png", width=1000, height=1000, res=110) ## Create the image file.
-layout(matrix(c(1, 2), nrow=2), heights=c(.5, 4)) ## Lay out the image.
-par(mar=rep(0, 4))
-plot.new() ## Specify a new plot, just in case.
-text(x=0.5, y=0.5, "Clinton word frequency in 9/26/2016 presidential debate", cex=2, font=2) ## Title.
-text(x=0.5, y=0.1, "Pioneer Press graphic by David H. Montgomery", cex=1) ## Subtitle/credit.
-wordcloud(clinton, max.words = 250, random.order= FALSE, colors=brewer.pal(8,"Dark2"), rot.per=0)
-dev.off()
-
-png("trump.png", width=1000, height=1000, res=110) ## Create the image file.
-layout(matrix(c(1, 2), nrow=2), heights=c(.5, 4)) ## Lay out the image.
-par(mar=rep(0, 4))
-plot.new() ## Specify a new plot, just in case.
-text(x=0.5, y=0.5, "Trump word frequency in 9/26/2016 presidential debate", cex=2, font=2) ## Title.
-text(x=0.5, y=0.1, "Pioneer Press graphic by David H. Montgomery", cex=1) ## Subtitle/credit.
-wordcloud(trump, max.words = 250, random.order= FALSE, colors=brewer.pal(8,"Dark2"), rot.per=0)
-dev.off()
-
-
-png("holt.png", width=1000, height=1000, res=110) ## Create the image file.
-layout(matrix(c(1, 2), nrow=2), heights=c(.5, 4)) ## Lay out the image.
-par(mar=rep(0, 4))
-plot.new() ## Specify a new plot, just in case.
-text(x=0.5, y=0.5, "Holt word frequency in 9/26/2016 presidential debate", cex=2, font=2) ## Title.
-text(x=0.5, y=0.1, "Pioneer Press graphic by David H. Montgomery", cex=1) ## Subtitle/credit.
-wordcloud(holt, max.words = 250, random.order= FALSE, colors=brewer.pal(8,"Dark2"), rot.per=0)
-dev.off()
+# For each of the three candidates... 
+for (i in 1:3) {
+	# Load the transcripts
+	assign(
+		names[i],
+		read.delim(
+			file=paste0(names[i],".txt"),
+			sep="\n", 
+			stringsAsFactors = F, 
+			header = FALSE))
+	
+	# Process the transcripts
+	assign(
+		names[i],
+		unlist(
+			lapply(get(names[i]), function(x) { str_split(x, "\n")})))
+	assign(names[i], Corpus(VectorSource(get(names[i]))))
+	assign(names[i], tm_map(get(names[i]), content_transformer(tolower))) ## Make everything lowercase
+	assign(names[i], tm_map(get(names[i]), removeWords,stopwords("english"))) ## Remove common English words like "and" and "the".
+	assign(names[i], tm_map(get(names[i]), removePunctuation)) ## Remove punctuation
+	assign(names[i], tm_map(get(names[i]), stripWhitespace)) ## Strip out unnecessary whitespace.
+	assign(paste0(names[i],".tdm"), TermDocumentMatrix(get(names[i]))) # Convert into the necessary format for a word cloud
+	
+	# Create the word cloud
+	png(paste0(names[i],".png"), width=1000, height=1000, res=110) # Create the image file
+	layout(matrix(c(1, 2), nrow=2), heights=c(.5, 4)) ## Lay out the image.
+	par(mar=rep(0, 4)) # Set the margins
+	plot.new() ## Specify a new plot, just in case.
+	text(x=0.5, y=0.5, paste(capitalize(names[i]),"word frequency in 9/26/2016 presidential debate"), cex=2, font=2) ## Title.
+	text(x=0.5, y=0.1, "Pioneer Press graphic by David H. Montgomery", cex=1) ## Subtitle/credit.
+	wordcloud(get(names[i]), max.words = 250, random.order= FALSE, colors=brewer.pal(8,"Dark2"), rot.per=0) # Make the actual word cloud
+	dev.off() # Print out the image.
+}
