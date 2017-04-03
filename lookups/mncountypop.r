a70cab64ef7ecf97da48c8906a007920e22cda4c
dhmontgomery <dhmontgomery@gmail.com>
2017-04-03 14:21:25 -0500

    Cleanup, fix, comments

--- lookups/mncountypop.r
+++ lookups/mncountypop.r
@@ -3,15 +3,14 @@ library(dplyr)
 library(readxl)
 
 url <- "https://mn.gov/admin/assets/mn_county_estimates_sdc_2015_tcm36-250306.xlsx" # Set the URL
-tmp = tempfile(fileext = ".xlsx")
-download.file(url, destfile = tmp, mode = "wb")
-mncountypop <- read_excel(tmp)[-88,-c(6:7)]
+tmp = tempfile(fileext = ".xlsx") # Create a temporary file to store the spreadsheet
+download.file(url, destfile = tmp, mode = "wb") # Download the URL into the temporary file
+mncountypop <- read_excel(tmp)[-88,-c(6:7)] # Clean the data
 
-colnames(mncountypop) <- c("FIPS", "FIPS.county", "county", "pop", "households", "pop.per.household")
-mncountypop$num <- row.names(mncountypop)
+colnames(mncountypop) <- c("FIPS", "FIPS.county", "county", "pop", "households", "pop.per.household") # Rename the columns
+mncountypop$num <- row.names(mncountypop) # Add a column with each county's number
 
-mncountypop$county <- gsub("St. ","Saint ",mncountypop$county)
-mncountypop$county <- gsub("qui ","Qui ",mncountypop$county)
+mncountypop$county <- gsub("St. ","Saint ",mncountypop$county) # Format Saint Louis County
+mncountypop$county <- gsub("qui ","Qui ",mncountypop$county) # Format Lac Qui Parle County
 
-mncountypop <- select(mncountypop, num, code, FIPS, county, pop, households, pop.per.household)
-mncountypop <- select(mncountypop,7,2,1,3,4,5,6)
+mncountypop <- select(mncountypop, num, code = FIPS.county, FIPS, county, pop, households, pop.per.household) # Select and reorder columns
