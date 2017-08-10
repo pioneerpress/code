882f4cbbdb6dfe29ba70e05799f528379453852f
David Montgomery <dhmontgomery@users.noreply.github.com>
2017-08-10 16:36:01 -0500

    Deleted an extraneous -

--- lookups/mncountypop.r
+++ lookups/mncountypop.r
@@ -5,7 +5,7 @@ library(readxl)
 url <- "https://mn.gov/admin/assets/mn_county_estimates_sdc_2016_tcm36-304002.xlsx" # Set the URL
 tmp = tempfile(fileext = ".xlsx") # Create a temporary file to store the spreadsheet
 download.file(url, destfile = tmp, mode = "wb") # Download the URL into the temporary file
-mncountypop <- read_excel(tmp)[-88,-c(1:3)] # Clean the data
+mncountypop <- read_excel(tmp)[-88,c(1:3)] # Clean the data
 
 colnames(mncountypop) <- c("countycode", "county", "pop") # Rename the columns
 mncountypop$num <- row.names(mncountypop) # Add a column with each county's number
